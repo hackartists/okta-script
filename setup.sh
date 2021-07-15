5c9329de89b30a9e25a0c78893303e64d8cc8f50
luffy-choi <luffy.choi@groundx.xyz>
2021-07-15 13:53:15 +0900

    remove dev and prod profiles

--- setup.sh
+++ setup.sh
@@ -14,20 +14,6 @@ factor = OKTA
 app-link = https://groundx.okta.com/home/amazon_aws/0oa3b7q2dEHRFlkab5d6/272
 base-url = groundx.okta.com
 duration = 43200
-
-[service-dev]
-username = $user@groundx.xyz
-factor = OKTA
-app-link = https://groundx.okta.com/home/amazon_aws/0oa3b7q2dEHRFlkab5d6/272
-base-url = groundx.okta.com
-duration = 43200
-
-[service-prod]
-username = $user@groundx.xyz
-factor = OKTA
-app-link = https://groundx.okta.com/home/amazon_aws/0oa10gs49r83z98Hy5d7/272
-base-url = groundx.okta.com
-duration = 43200
 EOT
 
 mkdir -p ~/.aws
@@ -36,32 +22,12 @@ cat <<EOT >> $aws_config
 [default]
 region = ap-northeast-2
 output=json
-
-[service-dev]
-region = ap-northeast-2
-output=json
-
-[service-prod]
-region = ap-northeast-2
-output=json
 EOT
 
 echo "Setting up Okta (default)..."
 okta-awscli --okta-profile default --profile default
 echo "Testing AWS CLI (default)"
 aws sts get-caller-identity
-
-echo ""
-echo "Setting up Okta (service-dev)..."
-okta-awscli --okta-profile service-dev --profile service-dev
-echo "Testing AWS CLI (service-dev)"
-aws sts --profile service-dev get-caller-identity
-
-echo ""
-echo "Setting up Okta (service-prod)..."
-okta-awscli --okta-profile service-prod --profile service-prod 
-echo "Testing AWS CLI (service-dev)"
-aws sts --profile service-prod get-caller-identity
 
 echo ""
 echo "Setting up Kubernetes Clusters for Development Team"
