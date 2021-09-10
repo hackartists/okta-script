e236069dfa01844788dbff89276648a4d4ddc7c6
hackartists <js.choi.85@gmail.com>
2021-09-10 18:01:24 +0900

    update qa cluster name

--- setup.sh
+++ setup.sh
@@ -34,14 +34,15 @@ echo "Setting up Kubernetes Clusters for Development Team"
 echo "Updating kubernetes config for DEV"
 aws --profile default eks update-kubeconfig --name service-dev --region ap-northeast-2
 
-echo "Updating kubernetes config for DEV(Old)"
-aws --profile default eks update-kubeconfig --name service-dev-common --region ap-northeast-2
+# echo "Updating kubernetes config for DEV(Old)"
+# aws --profile default eks update-kubeconfig --name service-dev-common --region ap-northeast-2
 
 echo "Updating kubernetes config for QA"
-aws --profile default eks update-kubeconfig --name service-qa-common --region ap-northeast-2
+aws --profile default eks update-kubeconfig --name service-qa --region ap-northeast-2
+# aws --profile default eks update-kubeconfig --name service-qa-common --region ap-northeast-2
 
 echo "Updating kubernetes config for PERF"
 aws --profile default eks update-kubeconfig --name service-qa-perf --region ap-northeast-2
 
-echo "Updating kubernetes config for PROD"
-aws --profile service-prod eks update-kubeconfig --name service-prod-common --region ap-northeast-2
+# echo "Updating kubernetes config for PROD"
+# aws --profile service-prod eks update-kubeconfig --name service-prod-common --region ap-northeast-2
